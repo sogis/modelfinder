288d8136ce71275ce43aa88722a3da3fd453988f
edigonzales <edi.gonzales@gmail.com>
2022-08-02 16:46:35 +0200

    try scheduler every 2 minutes

--- modelfinder-server/src/main/java/ch/so/agi/MainController.java
+++ modelfinder-server/src/main/java/ch/so/agi/MainController.java
@@ -169,7 +169,8 @@ public class MainController {
         return ServletUriComponentsBuilder.fromCurrentContextPath().build().toUriString();
     }
         
-    @Scheduled(cron="0 */2 * * *")
+    //@Scheduled(cron="0 */2 * * *")
+    @Scheduled(cron="*/2 * * * *")
     private void rebuildIndex() {
         System.out.println("******************REINDEX...");
         try {
